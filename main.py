656f6f98db3a4cde586f815890df01b8d3d15291
Rjoydip <joydipand@gmail.com>
2017-10-08 13:20:56 -0800

    python print using sys

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 
 import sys
-import zmq
+import zmq # pip install pyzmq
 import json
 
+# print message using sys
+def print(message):
+    sys.stdout.write(message)
+    return True
+
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect ("tcp://127.0.0.1:8688")
+rpc = context.socket(zmq.REQ)
+rpc.connect ("tcp://127.0.0.1:8688")
+
 print("Python RPC conncted to server...")
 
-socket.send_json({'cmd': 'conn', 'payload': 'success'})
-message = json.loads(socket.recv())
+rpc.send_json({'cmd': 'conn', 'payload': 'success'})
+message = json.loads(rpc.recv())
 
 print("Py message {}".format(message))
 
 try:
     if message['cmd'] == 'rpc':
-        print("Python RPC message {}".format(message))
-        socket.send_json({'cmd': 'rpc', 'paylod': '1'})
+        sys.stdout.write("Python RPC message {}".format(message))
+        rpc.send_json({'cmd': 'rpc', 'paylod': '1'})
     elif message['cmd'] == 'exit':
-        print("Python RPC message {}".format(message))
+        rpc.send_json("Python RPC message {}".format(message))
         sys.exit(0)
 except KeyboardInterrupt:
     pass
